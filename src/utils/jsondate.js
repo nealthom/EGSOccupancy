64fa4bb9290a6f2d93403b563a5b44c21034c6ea
Thomas Neal <nealthom@gmail.com>
2020-11-25 04:39:35 -0600

    updating data

--- src/utils/jsondate.js
+++ src/utils/jsondate.js
@@ -1,14 +1,13 @@
 import { date2ms } from "./date2ms";
 
 export const jsondate = (data, setData) => {
-  let newData = data;
+  let newData = [];
+  let dataIndex = 0;
   data.forEach((row, index) => {
     if (row["Business Date"]) {
-      newData[index]["Business Date"] = date2ms(row["Business Date"]);
-
-      setData(newData);
+      newData.push(row);
     }
   });
 
-  setData(data);
+  setData(newData);
 };
